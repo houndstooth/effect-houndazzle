1aa33b8feceaa5968c82fd83433850de2167bed3
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-17 17:28:22 +0900

    refactor shared square/tile code
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- components/dazzleSquare.js
+++ components/dazzleSquare.js
@@ -4,8 +4,8 @@ import solidDazzleSquare from './solidDazzleSquare'
 export default ({ origin, size, squareType, scaleFromGridCenter, substripeCount }) => {
 	size = size || 1
 
-	let originSubstripeDirection = squareType == "STRIPED_A" || squareType == "VERTICAL_SUBSTRIPES" ? "VERTICAL" : "HORIZONTAL"
-	let dazzleSquare = squareType == "STRIPED_A" || squareType == "STRIPED_B" ? stripedDazzleSquare : solidDazzleSquare
+	let originSubstripeDirection = squareType === "STRIPED_A" || squareType === "VERTICAL_SUBSTRIPES" ? "VERTICAL" : "HORIZONTAL"
+	let dazzleSquare = squareType === "STRIPED_A" || squareType === "STRIPED_B" ? stripedDazzleSquare : solidDazzleSquare
 
 	dazzleSquare({ origin, size, originSubstripeDirection, scaleFromGridCenter, substripeCount })
 }
