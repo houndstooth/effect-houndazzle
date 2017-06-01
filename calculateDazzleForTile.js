0a6d50ce50f712b3d94f4de5ca66d703ab0364fd
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-06-01 15:47:56 +0900

    simplify dazzle args
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- calculateDazzleForTile.js
+++ calculateDazzleForTile.js
@@ -2,14 +2,14 @@ import state from '../shared/state/state'
 import gridUtilities from '../shared/utilities/gridUtilities'
 import colorUtilities from '../shared/utilities/colorUtilities'
 
-export default ({ origin, dazzleColors, dazzleOrientations: initialDazzleOrientations }) => {
+export default ({ origin, initialDazzle }) => {
 	const { color, orientation } = state.shared.color.houndazzle
-	dazzleColors = colorUtilities.calculateColors({ origin, colors: dazzleColors, color })
-	const dazzleOrientations = initialDazzleOrientations || gridUtilities.calculateSetForTile({
+	const colors = colorUtilities.calculateColors({ origin, colors: initialDazzle && initialDazzle.colors, color })
+	const orientations = initialDazzle && initialDazzle.orientations || gridUtilities.calculateSetForTile({
 			origin,
 			grid: orientation,
 			gccOn: state.shared.stripeCount.mode === 'GINGHAM_CHEVRON_CONTINUUM'
 		})
 
-	return { dazzleColors, dazzleOrientations }
+	return { colors, orientations }
 }
