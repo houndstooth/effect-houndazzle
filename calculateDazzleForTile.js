f720bcf825262786b91374e9bdc78f15928b1705
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-06-01 16:32:35 +0900

    use address for tile id and determinations thereof, origin for canvas pixel and rendering manipulations thereof
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- calculateDazzleForTile.js
+++ calculateDazzleForTile.js
@@ -2,11 +2,11 @@ import state from '../shared/state/state'
 import gridUtilities from '../shared/utilities/gridUtilities'
 import colorUtilities from '../shared/utilities/colorUtilities'
 
-export default ({ origin, initialDazzle }) => {
+export default ({ address, initialDazzle }) => {
 	const { color, orientation } = state.shared.color.houndazzle
-	const colors = colorUtilities.calculateColors({ origin, colors: initialDazzle && initialDazzle.colors, color })
+	const colors = colorUtilities.calculateColors({ address, colors: initialDazzle && initialDazzle.colors, color })
 	const orientations = initialDazzle && initialDazzle.orientations || gridUtilities.calculateSetForTile({
-			origin,
+			address,
 			grid: orientation,
 			gccOn: state.shared.stripeCount.mode === 'GINGHAM_CHEVRON_CONTINUUM'
 		})
