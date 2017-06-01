fa6f3acc829ea1660ee19ec097cd22a7282d7c4d
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-06-01 14:17:03 +0900

    incremental improvments to var names and arg patterns
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- calculateHoundazzleSolidTileSubstripeCoordinates.js
+++ calculateHoundazzleSolidTileSubstripeCoordinates.js
@@ -2,24 +2,24 @@ import flipXAndY from './flipXAndY'
 
 export default ({ origin, sizedUnit, substripeIndex, coordinatesFunctionArguments }) => {
 	const { substripeUnit, orientation } = coordinatesFunctionArguments
-	const currentSubstripePosition = substripeIndex * substripeUnit
-	const nextSubstripePosition = currentSubstripePosition + substripeUnit
+	const substripeStart = substripeIndex * substripeUnit
+	const substripeEnd = substripeStart + substripeUnit
 
 	let coordinates = [
 		[
-			origin[ 0 ] + currentSubstripePosition,
+			origin[ 0 ] + substripeStart,
 			origin[ 1 ]
 		],
 		[
-			origin[ 0 ] + nextSubstripePosition,
+			origin[ 0 ] + substripeEnd,
 			origin[ 1 ]
 		],
 		[
-			origin[ 0 ] + nextSubstripePosition,
+			origin[ 0 ] + substripeEnd,
 			origin[ 1 ] + sizedUnit
 		],
 		[
-			origin[ 0 ] + currentSubstripePosition,
+			origin[ 0 ] + substripeStart,
 			origin[ 1 ] + sizedUnit
 		],
 	]
--- calculateSubstripeStripeUnionCoordinates.js
+++ calculateSubstripeStripeUnionCoordinates.js
@@ -1,79 +1,79 @@
 import flipXAndY from './flipXAndY'
 
 const calculateSubstripeStripeUnionCoordinates = ({
-	currentStripePosition,
-	nextStripePosition,
-	currentSubstripePosition,
+	stripeStart,
+	stripeEnd,
+	substripeStart,
 	substripeUnit,
 	sizedUnit,
 	origin
 }) => {
-	const nextSubstripePosition = currentSubstripePosition + substripeUnit
+	const nextSubstripePosition = substripeStart + substripeUnit
 
 	const coordinates = []
 
 	// top left (which may also turn out to be top right)
-	if (currentStripePosition > sizedUnit) {
+	if (stripeStart > sizedUnit) {
 		coordinates.push([
 			origin[ 0 ] + sizedUnit,
-			origin[ 1 ] + currentSubstripePosition + currentStripePosition - sizedUnit
+			origin[ 1 ] + substripeStart + stripeStart - sizedUnit
 		])
-	} else if (currentStripePosition >= 0) {
+	} else if (stripeStart >= 0) {
 		coordinates.push([
-			origin[ 0 ] + currentStripePosition,
-			origin[ 1 ] + currentSubstripePosition
+			origin[ 0 ] + stripeStart,
+			origin[ 1 ] + substripeStart
 		])
 	} else {
 		coordinates.push([
 			origin[ 0 ],
-			origin[ 1 ] + currentSubstripePosition
+			origin[ 1 ] + substripeStart
 		])
 	}
 
 	// possibly top right, and possibly middle right
-	if (nextStripePosition <= sizedUnit) {
+	if (stripeEnd <= sizedUnit) {
 		coordinates.push([
-			origin[ 0 ] + nextStripePosition,
-			origin[ 1 ] + currentSubstripePosition
+			origin[ 0 ] + stripeEnd,
+			origin[ 1 ] + substripeStart
 		])
 	} else {
-		if (currentStripePosition < sizedUnit) {
+		if (stripeStart < sizedUnit) {
 			coordinates.push([
 				origin[ 0 ] + sizedUnit,
-				origin[ 1 ] + currentSubstripePosition
+				origin[ 1 ] + substripeStart
 			])
 		}
-		if (nextStripePosition < sizedUnit + substripeUnit) {
+		if (stripeEnd < sizedUnit + substripeUnit) {
 			coordinates.push([
 				origin[ 0 ] + sizedUnit,
-				origin[ 1 ] + currentSubstripePosition + nextStripePosition - sizedUnit
+				origin[ 1 ] + substripeStart + stripeEnd - sizedUnit
 			])
 		}
 	}
 
 	// bottom right, and potentially early return to skip last section
-	if (nextStripePosition - substripeUnit >= sizedUnit) {
+	if (stripeEnd - substripeUnit >= sizedUnit) {
 		coordinates.push([
 			origin[ 0 ] + sizedUnit,
 			origin[ 1 ] + nextSubstripePosition
 		])
-	} else if (nextStripePosition - substripeUnit >= 0) {
+	} else if (stripeEnd - substripeUnit >= 0) {
 		coordinates.push([
-			origin[ 0 ] + nextStripePosition - substripeUnit,
+			origin[ 0 ] + stripeEnd - substripeUnit,
 			origin[ 1 ] + nextSubstripePosition
 		])
 	} else {
 		coordinates.push([
 			origin[ 0 ],
-			origin[ 1 ] + currentSubstripePosition + nextStripePosition
+			origin[ 1 ] + substripeStart + stripeEnd
 		])
 		return coordinates
 	}
 
 	// if not skipping this section due to early return, bottom left, and possibly middle left
-	if (currentStripePosition - substripeUnit > 0) {
+	if (stripeStart - substripeUnit > 0) {
 		coordinates.push([
-			origin[ 0 ] + currentStripePosition - substripeUnit,
+			origin[ 0 ] + stripeStart - substripeUnit,
 			origin[ 1 ] + nextSubstripePosition
 		])
 	} else {
@@ -81,10 +81,10 @@ const calculateSubstripeStripeUnionCoordinates = ({
 			origin[ 0 ],
 			origin[ 1 ] + nextSubstripePosition
 		])
-		if (currentStripePosition > 0) {
+		if (stripeStart > 0) {
 			coordinates.push([
 				origin[ 0 ],
-				origin[ 1 ] + currentSubstripePosition + currentStripePosition
+				origin[ 1 ] + substripeStart + stripeStart
 			])
 		}
 	}
@@ -93,25 +93,25 @@ const calculateSubstripeStripeUnionCoordinates = ({
 }
 
 export default ({ origin, sizedUnit, stripeIndex, substripeIndex, coordinatesFunctionArguments }) => {
-	const { currentPositionAlongPerimeter, nextPositionAlongPerimeter, substripeUnit, orientation } = coordinatesFunctionArguments
-	const currentSubstripePosition = substripeIndex * substripeUnit
+	const { stripeStart: initialStripeStart, stripeEnd: initialStripeEnd, substripeUnit, orientation } = coordinatesFunctionArguments
+	const substripeStart = substripeIndex * substripeUnit
 
-	const currentStripePosition = currentPositionAlongPerimeter * sizedUnit - currentSubstripePosition
-	const nextStripePosition = nextPositionAlongPerimeter * sizedUnit - currentSubstripePosition
+	const stripeStart = initialStripeStart * sizedUnit - substripeStart
+	const stripeEnd = initialStripeEnd * sizedUnit - substripeStart
 
 	// this stripe is completely off the right edge of the substripe
-	if (currentStripePosition - substripeUnit >= sizedUnit) return
+	if (stripeStart - substripeUnit >= sizedUnit) return
 	// this stripe is completely off the left edge of the substripe
-	if (nextStripePosition <= 0) return
+	if (stripeEnd <= 0) return
 
 	let coordinates = calculateSubstripeStripeUnionCoordinates({
-		currentStripePosition,
+		stripeStart,
 		stripeIndex,
-		currentSubstripePosition,
+		substripeStart,
 		sizedUnit,
 		substripeUnit,
 		origin,
-		nextStripePosition
+		stripeEnd
 	})
 
 	if (orientation === "VERTICAL") coordinates = flipXAndY({ coordinates, origin })
