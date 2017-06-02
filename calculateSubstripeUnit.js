b93f41e3dd606fa0c9022fcce1113e65bbf084ba
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-06-02 12:39:40 +0900

    delay calculation of origin from address until later, so that address-based calculations can be made later
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

---
+++ calculateSubstripeUnit.js
@@ -0,0 +1,3 @@
+import transpositionUtilities from '../shared/utilities/transpositionUtilities'
+
+export default ({ substripeCount, size }) => transpositionUtilities.calculateSizedUnit({ size }) / substripeCount
--- houndazzleShapeWrapper.js
+++ houndazzleShapeWrapper.js
@@ -4,23 +4,24 @@ import shape from '../shared/components/shape'
 import wrappedIndex from '../shared/utilities/wrappedIndex'
 import substripeModulus from './substripeModulus'
 import houndazzleShapes from './houndazzleShapes'
+import calculateSubstripeUnit from './calculateSubstripeUnit'
 
-export default ({ origin, colors, rotation, sizedUnit, stripeIndex, dazzle, coordinatesOptions, coordinatesFunction }) => {
+export default ({ address, size, colors, rotation, stripeIndex, dazzle, coordinatesOptions, coordinatesFunction }) => {
 	const { substripeOfSquare, substripeOfStripe } = houndazzleShapes
 	coordinatesFunction = coordinatesOptions ? substripeOfStripe : substripeOfSquare
 
 	const { substripeCount } = state.shared.colorConfig.houndazzle
 	coordinatesOptions = coordinatesOptions || {}
-	coordinatesOptions.substripeUnit = sizedUnit / substripeCount
+	coordinatesOptions.substripeUnit = calculateSubstripeUnit({ substripeCount, size })
 	coordinatesOptions.orientation = wrappedIndex({ array: dazzle.orientations, index: stripeIndex })
 
 	iterator(substripeCount).forEach(substripeIndex => {
 		coordinatesOptions.substripeIndex = substripeIndex
 		shape({
-			origin,
+			address,
+			size,
 			colors: substripeModulus({ substripeIndex, nonDazzle: colors, dazzle: dazzle.colors }),
 			rotation,
-			sizedUnit,
 			stripeIndex,
 			coordinatesFunction,
 			coordinatesOptions
