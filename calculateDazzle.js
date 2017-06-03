6e7f2663b5a74e281834749f7fe232481c726057
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-06-03 12:49:40 +0900

    now that houndsmorphosis no longer needs to decide upfront, no need for initial dazzle

--- calculateDazzle.js
+++ calculateDazzle.js
@@ -2,10 +2,10 @@ import state from '../shared/state/state'
 import gridUtilities from '../shared/utilities/gridUtilities'
 import colorUtilities from '../shared/utilities/colorUtilities'
 
-export default ({ address, initialDazzle }) => {
+export default ({ address }) => {
 	const { colorConfig, orientationConfig } = state.colorConfig.houndazzle
-	const colors = colorUtilities.calculateColors({ address, colors: initialDazzle && initialDazzle.colors, colorConfig })
-	const orientations = initialDazzle && initialDazzle.orientations || gridUtilities.calculateSetForTile({
+	const colors = colorUtilities.calculateColors({ address, colorConfig })
+	const orientations = gridUtilities.calculateSetForTile({
 			address,
 			config: orientationConfig,
 			gccOn: state.stripeCountConfig.mode === 'GINGHAM_CHEVRON_CONTINUUM'
