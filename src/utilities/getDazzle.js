ea3e592415f6b5b65b3b6fae44fb17f0a6d6fcd5
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-07-26 13:27:17 +0900

    eliminate unnecessary tier of state hierarchy. life found a way to reset keys of a module

--- src/utilities/getDazzle.js
+++ src/utilities/getDazzle.js
@@ -3,7 +3,7 @@ import colorUtilities from '../../../../src/utilities/colorUtilities'
 import store from '../../../../store'
 
 export default ({ gridAddress }) => {
-	const { colorSettings, orientationSettings } = store.currentState.mainHoundstooth.basePattern.colorSettings.houndazzle
+	const { colorSettings, orientationSettings } = store.mainHoundstooth.basePattern.colorSettings.houndazzle
 	const tileColors = colorUtilities.getColorsForTile({ gridAddress, colorSettings })
 	const tileOrientations = componentUtilities.getSetForTile({
 		gridAddress,
