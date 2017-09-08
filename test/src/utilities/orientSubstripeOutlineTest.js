be90d3d3a6aa6e0c9d3f82aba7ffd23937a4ffeb
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-09-08 22:00:18 +0900

    unused import

--- test/src/utilities/orientSubstripeOutlineTest.js
+++ test/src/utilities/orientSubstripeOutlineTest.js
@@ -1,6 +1,5 @@
 import orientSubstripeOutline from '../../../src/utilities/orientSubstripeOutline'
 import rotationUtilities from '../../../../../src/utilities/rotationUtilities'
-import tileCenter from '../../../../../src/components/tileCenter'
 
 describe('orient substripe outline', () => {
 	it('orients the substripe outline according to the index of the solid color it would have been', () => {
