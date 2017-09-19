1c8b57dc04fddf68299ef9bfc72f69d9352e8d83
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-09-19 13:28:42 +0900

    use default exports in code utilities

--- src/textures/substripeTexture.js
+++ src/textures/substripeTexture.js
@@ -1,12 +1,12 @@
 import houndazzleConstants from '../houndazzleConstants'
-import codeUtilities from '../../../../src/utilities/codeUtilities'
+import { iterator } from '../../../../src/utilities/codeUtilities'
 import components from '../components'
 
 export default ({ context, tileColorIndices, tileOrigin, tileSize, shapeColorIndex }) => {
 	const substripeCount = houndazzleConstants.SUBSTRIPE_COUNT * 2
 	const colorsCount = tileColorIndices.length
 
-	codeUtilities.iterator(substripeCount).forEach(substripeIndex => {
+	iterator(substripeCount).forEach(substripeIndex => {
 		components.substripe({
 			context,
 			tileOrigin,
