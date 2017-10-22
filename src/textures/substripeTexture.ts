afc13cf8718703c1524bc6a2dbaffdea8189dc24
Douglas <douglas.blumeyer@gmail.com>
2017-10-22 14:58:14 +0800

    make some sense of solid vs texture

--- src/textures/substripeTexture.ts
+++ src/textures/substripeTexture.ts
@@ -1,15 +1,9 @@
-import { Context, Coordinate, TileColorIndices, Unit } from '../../../../src'
+import { RenderTexture } from '../../../../src'
 import { iterator } from '../../../../src/utilities/codeUtilities'
 import { substripe } from '../components'
 import { SUBSTRIPE_COUNT, SUFFICIENT_FACTOR_TO_GUARANTEE_TILE_COVERAGE } from '../constants'
 
-const substripeTexture: (_: {
-	context: Context,
-	shapeColorIndex: number,
-	tileColorIndices: TileColorIndices,
-	tileOrigin: Coordinate,
-	tileSize: Unit,
-}) => void = ({ context, shapeColorIndex, tileColorIndices, tileOrigin, tileSize }) => {
+const substripeTexture: RenderTexture = ({ context, shapeColorIndex, tileColorIndices, tileOrigin, tileSize }) => {
 	const substripeCount = SUBSTRIPE_COUNT * SUFFICIENT_FACTOR_TO_GUARANTEE_TILE_COVERAGE
 	const colorsCount = tileColorIndices.length
 
