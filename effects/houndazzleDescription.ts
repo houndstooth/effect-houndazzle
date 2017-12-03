63ef6fccd1458da5913e8e080f2e0f0b243668db
Douglas <douglas.blumeyer@gmail.com>
2017-12-03 21:27:08 +0900

    effect descriptions

---
+++ effects/houndazzleDescription.ts
@@ -0,0 +1,15 @@
+const HOUNDAZZLE_DESCRIPTION: string = [
+	'--- houndazzle ---',
+	'',
+	'Replaces the color a tile would be with a striped texture.',
+	'This texture\'s stripes are smaller than the stripes',
+	'of the pattern itself, so are called substripes.',
+	'Substripe color cycles through every color in the original set.',
+	'So, each section of the pattern has the same overall color.',
+	'The only distinguishing element is the orientation of the substripes.',
+	'This depends on the color count. With only two colors,',
+	'the texture will compare horizontal against vertical.',
+	'With more colors, well... you\'ll see.',
+].join('\n')
+
+export default HOUNDAZZLE_DESCRIPTION
--- effects/houndazzleEffect.ts
+++ effects/houndazzleEffect.ts
@@ -1,5 +1,6 @@
 import { NamedEffect } from '../../../src'
 import { substripeTexture } from '../pattern'
+import HOUNDAZZLE_DESCRIPTION from './houndazzleDescription'
 
 const houndazzleEffect: NamedEffect = {
 	basePattern: {
@@ -7,6 +8,7 @@ const houndazzleEffect: NamedEffect = {
 			executeTexture: substripeTexture.default,
 		},
 	},
+	description: HOUNDAZZLE_DESCRIPTION,
 	name: 'houndazzle',
 }
 
