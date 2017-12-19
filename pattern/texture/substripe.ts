02bb304e37c4c43068f943d2101151795cec1297
cmloegcmluin <douglas.blumeyer@gmail.com>
2017-12-19 16:02:09 +0900

    do not have vars named just 'params'

--- pattern/texture/substripe.ts
+++ pattern/texture/substripe.ts
@@ -4,8 +4,8 @@ import substripeOutline from './substripeOutline'
 import { SubstripeParams } from './types'
 
 const substripe: (_: SubstripeParams) => void =
-	(params: SubstripeParams): void => {
-		const { tileSize, shapeColorIndex, substripeIndex, substripeCount }: SubstripeParams = params
+	(substripeParams: SubstripeParams): void => {
+		const { tileSize, shapeColorIndex, substripeIndex, substripeCount }: SubstripeParams = substripeParams
 		let outline: Outline = substripeOutline({ tileSize, substripeIndex, substripeCount })
 		outline = orientSubstripeOutline({ shapeColorIndex, outline })
 		solid.default({ outline, shapeColorIndex: to.ShapeColorIndex(substripeIndex) })
